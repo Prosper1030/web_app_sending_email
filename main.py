976aa1f1467d6a9bb8553a7968952072070e5192
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-19 23:41:55 +0800

    Add description, url, image

--- main.py
+++ main.py
@@ -13,18 +13,26 @@ with col2:
     我是林禹安，現在在練習寫程式，以下是我的作品集
     """
     st.info(content)
+
 text = """
 Below you can find some of the apps I have built in Python.
 Nice to see you.
 """
 st.write(text)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
+
 with col3:
     for index, row in df[:10].iterrows():
         st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")    #[]()之間不能有空格
 
 with col4:
     for index, row in df[10:].iterrows():
         st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")

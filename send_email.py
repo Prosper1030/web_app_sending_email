e938e3af15cb5a71518de7101898e1c567b891de
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-20 12:38:20 +0800

    Send email using the form

--- send_email.py
+++ send_email.py
@@ -1,17 +1,15 @@
 import smtplib, ssl
+import os
 
-host = "smtp.gmail.com"
-port = 465
-username = "1030railgunjudeglight@gmail.com"
-password = "ddmnhkmpbufsqgfo"
-receiver = "1030railgunjudeglight@gmail.com"
 
-message ="""\
-Subject: test
-hello
-"""
-context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "1030railgunjudeglight@gmail.com"
+    password = os.getenv("PASSWORD")   # 從環境變量中取得密碼
+    receiver = "1030railgunjudeglight@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message.encode("utf-8"))    # 用utf-8 解碼
